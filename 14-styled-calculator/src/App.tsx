3e93f8e96ea59342ffa062b67ac448b316ecd737
Beka Zandukeli <bekazandukeli@gmail.com>
2022-12-22 22:05:45 +0200

    adds theme and styled input component

--- 14-styled-calculator/src/App.tsx
+++ 14-styled-calculator/src/App.tsx
@@ -1,4 +1,11 @@
 import React, { useEffect, useState } from 'react';
+import { Helmet } from 'react-helmet';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from './components/GlobalStyles';
+import { Input } from './components/Input';
+import { defaultTheme } from './themes/defaultTheme';
+
+const MAX_PERSONS = 9;
 
 export default function Calculator() {
   const [bill, setBill] = useState<number | undefined>(undefined);
@@ -25,88 +32,107 @@ export default function Calculator() {
   }, [people]);
 
   return (
-    <div className="App">
-      Bill:
-      <input
-        placeholder="bill"
-        type="number"
-        min={0}
-        value={bill}
-        onChange={(e) => {
-          setBill(e.target.valueAsNumber);
-        }}
-      />
-      <div>
-        <button
-          onClick={() => {
-            setTip(0.05);
-          }}
-        >
-          5%
-        </button>
-        <button
-          onClick={() => {
-            setTip(0.1);
-          }}
-        >
-          10%
-        </button>
-        <button
-          onClick={() => {
-            setTip(0.15);
-          }}
-        >
-          15%
-        </button>
-        <button
-          onClick={() => {
-            setTip(0.25);
-          }}
-        >
-          25%
-        </button>
-        <button
-          onClick={() => {
-            setTip(0.5);
-          }}
-        >
-          50%
-        </button>
-        <input
-          placeholder="custom"
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyles />
+      <Helmet>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Space+Mono:wght@700&display=swap"
+          rel="stylesheet"
+        />
+      </Helmet>
+
+      <div className="App">
+        Bill:
+        <Input
+          iconType="bill"
+          placeholder="0"
           type="number"
           min={0}
-          max={100}
-          value={tip && tip * 100}
+          value={bill}
           onChange={(e) => {
-            setTip(e.target.valueAsNumber / 100);
+            if (e.target.value.length < 9) {
+              setBill(e.target.valueAsNumber);
+            }
           }}
         />
+        <div>
+          <button
+            onClick={() => {
+              setTip(0.05);
+            }}
+          >
+            5%
+          </button>
+          <button
+            onClick={() => {
+              setTip(0.1);
+            }}
+          >
+            10%
+          </button>
+          <button
+            onClick={() => {
+              setTip(0.15);
+            }}
+          >
+            15%
+          </button>
+          <button
+            onClick={() => {
+              setTip(0.25);
+            }}
+          >
+            25%
+          </button>
+          <button
+            onClick={() => {
+              setTip(0.5);
+            }}
+          >
+            50%
+          </button>
+          <Input
+            placeholder="Custom"
+            type="number"
+            min={0}
+            value={tip && tip * 100}
+            onChange={(e) => {
+              if (e.target.value.length < 4) {
+                setTip(e.target.valueAsNumber / 100);
+              }
+            }}
+          />
+        </div>
+        People:
+        <Input
+          iconType="person"
+          placeholder="0"
+          type="number"
+          value={people}
+          min={0}
+          onKeyDown={(e) => {
+            if (e.key === '.') {
+              e.preventDefault();
+            }
+          }}
+          onChange={(e) => {
+            if (e.target.value.length < MAX_PERSONS) {
+              setPeople(e.target.valueAsNumber);
+            }
+          }}
+        />
+        <div style={{ color: 'red' }}>
+          {peopleError ? `Can't Be Zero` : null}
+        </div>
+        <div>
+          tip amount / person:
+          {showTip ? tipAmount : '0.00'}
+        </div>
+        <div>
+          total / person:
+          {showTotal ? totalPerPerson : '0.00'}
+        </div>
       </div>
-      People:
-      <input
-        placeholder="number of people"
-        type="number"
-        value={people}
-        min={0}
-        onKeyDown={(e) => {
-          if (e.key === '.') {
-            e.preventDefault();
-          }
-        }}
-        onChange={(e) => {
-          setPeople(e.target.valueAsNumber);
-        }}
-      />
-      <div style={{ color: 'red' }}>{peopleError ? `Can't Be Zero` : null}</div>
-      <div>
-        tip amount / person:
-        {showTip ? tipAmount : '0.00'}
-      </div>
-      <div>
-        total / person:
-        {showTotal ? totalPerPerson : '0.00'}
-      </div>
-    </div>
+    </ThemeProvider>
   );
 }
