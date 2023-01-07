7aa77f8643d1c5de1f5961a9dd22c8aecb65debb
Beka Zandukeli <bekazandukeli@Bekas-MacBook-Pro.local>
2023-01-07 21:23:36 +0200

    lecture 17

---
+++ 17-forms/src/App.tsx
@@ -0,0 +1,74 @@
+import { useRef } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import Input from "./components/Input";
+
+function Component(props:any) {
+  return <div>{props.name}</div>
+}
+
+let data = () => ({
+  name: 'Nika',
+  as: 'Nika',
+  nasdasame: 'Nika',
+  asd: 'Nika',
+  nasdame: 'Nika',
+  asdasd: 'Nika',
+  as234d: 'Nika',
+});
+
+type Inputs = {
+  name: string;
+  surname: string;
+  email: string;
+}
+
+function App() {
+  const { register, handleSubmit, watch, formState: {errors} } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data);
+  }
+  const nameInputRef = useRef(null);
+
+  console.log('Errors: ', errors);
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input placeholder="surname" {...register('surname')} />
+
+        <input
+          placeholder="name" 
+          {...register('name', {pattern: {
+            value: /^[A-Za-z]+$/i,
+            message: 'მხოლოდ ასოები ფლიიიზ'
+          }})}
+          
+        />
+        {errors.name && <p style={{color: 'red'}}>{errors.name.message}</p>}
+        
+        <Input
+          color="red"
+           placeholder="email" 
+           {...register( 'email', 
+           {
+            required: 'აუცილებლად ჩასაწერი!',
+            pattern: {
+              value: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+              message: "მეილი არავალიდურია!"
+            }
+           }
+           )}
+        />
+        {errors.email && <p style={{color: 'red'}}>{errors.email.message}</p>}
+
+        <button type="submit">Register</button>
+      </form>
+      
+      {/* <Component {...data()} /> */}
+
+    </div>
+  )
+}
+
+export default App
---
+++ 17-forms/src/components/Input.tsx
@@ -0,0 +1,13 @@
+import React, {ComponentProps} from 'react'
+
+type Props = ComponentProps<"input"> & {color: string};
+
+function Input({color, ...otherProps }: Props, ref: any) {
+  return (
+    <div style={{borderColor: color, borderWidth: 1, borderStyle: 'solid'}}>
+      <input ref={ref} {...otherProps} />
+    </div>
+  );
+}
+
+export default React.forwardRef(Input);
