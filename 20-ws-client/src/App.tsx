0b30056681f3b49c5ed20053d416cdff44dbaa73
bekazandukeli <bekazandukeli@gmail.com>
2023-01-17 21:16:28 +0200

    adds lecture 20 code

---
+++ 20-ws-client/src/App.tsx
@@ -0,0 +1,64 @@
+import { useEffect, useRef, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const connectionRef = useRef<WebSocket | null>(null);
+  
+  useEffect(() => {
+    const connection = new WebSocket('ws://localhost:7071');
+    // ვინახავთ ref-ს ქონეშენის ობიექტზე
+    connectionRef.current = connection;
+
+    connection.onopen = () => {
+      console.log('კავშირი დამყარდა!')
+    }
+
+    connection.onclose = () => {
+      console.log('კავშირი დაიხურა!')
+    };
+
+    connection.onerror = (ev) => {
+      console.error(ev);
+    }
+
+    connection.onmessage = (event: MessageEvent<string>): any => {
+      const message = JSON.parse(event.data) as {name: string; age: number;};
+
+      console.log(message);
+    }
+
+    // ეს იგივეა რაც ზემოთ onmessage
+    // connection.addEventListener('message', (event: MessageEvent<string>) => {
+    //   const message = JSON.parse(event.data) as {name: string; age: number;};
+
+    //   console.log(message);
+    // })
+    
+    return () => {
+      connection.close();
+    }
+  }, [])
+
+  return (
+    <div className="App">
+      <button onClick={() => {
+        connectionRef.current?.send(
+          {
+            clientName: 'Zura'
+          }
+        )
+      }}>
+        send something to ws
+      </button>
+
+      <button onClick={() => {
+        connectionRef.current?.close()
+      }}>
+        close connection
+      </button>
+    </div>
+  )
+}
+
+export default App
